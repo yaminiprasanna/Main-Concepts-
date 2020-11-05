547ea7a34425419bbf23ca5725a8b7a4a03821d5
yaminiprasanna <34371375+yaminiprasanna@users.noreply.github.com>
2020-11-05 18:26:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import Element from './components/Element'
+import Element1 from './components/Element1'
+//import Functional from './components/Functional'
+function App() {
+  
+  return (
+    <div className="App">
+    
+     {/* <Functional/> */}
+    
+    </div>
+    
+  );
+}
+
+export default App;
